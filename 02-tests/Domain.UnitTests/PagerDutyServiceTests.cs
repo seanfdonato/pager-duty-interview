dc35617b870d2109a240971e478122fbdab6895c
seanfdonato <39416945+seanfdonato@users.noreply.github.com>
2023-09-02 16:44:46 +0100

    Added Test project

---
+++ 02-tests/Domain.UnitTests/PagerDutyServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Core.Interfaces;
+using Moq;
+
+namespace Domain.UnitTests
+{
+    public class PagerDutyServiceTests
+	{
+		public PagerDutyServiceTests()
+		{
+            
+		}
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
